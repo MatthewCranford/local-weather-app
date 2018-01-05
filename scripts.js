b7e906adb8733d7748694502260349a063fc4aeb
Matt <matthewcranford314@gmail.com>
2018-01-04 17:25:19 -0800

    Add load timer and optimize images

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
 
+  getLocation();
 
   function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success); 
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var url = createUrl(position);
+        getData(url);  
+      }); 
     }
     else {
       document.getElementById("user-location").innerHTML = "Geolocation is not supported by this browser.";
     }
-  }
-
-  function success(position) {
-    var url = createUrl(position);
-    getData(url);  
   }
 
   function createUrl(position) {
@@ -20,11 +19,10 @@ $(document).ready(function(){
     return url;
   }
 
-
   function getData(url) {
     $.getJSON(url, function(data) {
       var id = data.weather[0].id;
-      renderBackground(id);
+      assignImage(id);
 
       var userLocation = document.getElementById("user-location");
       var weatherTemp = document.getElementById("weather-temp");
@@ -35,51 +33,60 @@ $(document).ready(function(){
       weatherTemp.innerHTML = getFahrenheit(data.main.temp);
       weatherIcon.src = data.weather[0].icon;
       weatherDescription.innerHTML = data.weather[0].main;
-   
-    });
+    }); 
   }
 
-  function renderBackground(id) {
-
+  function assignImage(id) {
     var gallery = [
-      "https://www.dropbox.com/s/mge7awopq7wz33s/pexels-photo-346529.jpeg?raw=1", // clear sky, id: 800
-      "https://www.dropbox.com/s/zi1r7ywe861kjeu/pexels-photo-414491.jpeg?raw=1", // clouds, id: 801-804
-      "https://www.dropbox.com/s/ehddchvdoabgdhy/pink-leaf-2779535_960_720.jpg?raw=1", // drizzle, id: 300-321
-      "https://www.dropbox.com/s/l9vixcqnqqpns7e/view-to-rain-from-wooden-veranda.jpg?raw=1", // rain, id: 500-531
-      "https://www.dropbox.com/s/86d4t39uuuvgykh/Thunderstorm_over_Corfu.jpg?raw=1", // lightning, id: 200-232
-      "https://www.dropbox.com/s/mgfzphclt29q0it/Forested_hills_in_Lysekil_in_fog_-_wide_-_B%26W.jpg?raw=1", // fog, id: 701-781
-      "https://www.dropbox.com/s/mmrxhzrf38qg7a2/snow-landscape-trees-winter.jpg?raw=1", // snow id: 600-622
+      "https://www.dropbox.com/s/j2l7jvk8y5mfohs/clear.jpg?raw=1", // clear sky, id: 800
+      "https://www.dropbox.com/s/c82bxoscfvvwawm/clouds.jpg?raw=1", // clouds, id: 801-804
+      "https://www.dropbox.com/s/cgsa9vwwj4hcg51/drizzle.jpg?raw=1", // drizzle, id: 300-321
+      "https://www.dropbox.com/s/bjq72yjbaop6230/rain.jpg?raw=1", // rain, id: 500-531
+      "https://www.dropbox.com/s/24toivyoo7do5x2/storm.jpg?raw=1", // lightning, id: 200-232
+      "https://www.dropbox.com/s/u3bwainyw93ibbd/fog.jpg?raw=1", // fog, id: 701-781
+      "https://www.dropbox.com/s/n2p2mrwaxwml2u2/snow.jpg?raw=1", // snow id: 600-622
     ]
-
-    // Assign background image based on id range
+    
+    // assign background image based on id range
     switch(true) {
       case id === 800: // clear skies
-        $("body").css("background-image", "url("+gallery[0]+")");
+        renderImage(gallery[0]);
         break;
       case id >= 801 && id <= 804: // cloudy
-        $("body").css("background-image", "url("+gallery[1]+")");
+        renderImage(gallery[1]);
         break;
       case id >= 300 && id <= 321: // drizzle
-        $("body").css("background-image", "url("+gallery[2]+")");
+        renderImage(gallery[2]);
         break; 
       case id >= 500 && id <= 531: // rain
-        $("body").css("background-image", "url("+gallery[3]+")");
+       renderImage(gallery[3]);
         break;   
       case id >= 200 && id <= 232: // lightning
-        $("body").css("background-image", "url("+gallery[4]+")");
+        renderImage(gallery[4]);
         break;   
       case id >= 701 && id <= 781: // fog
-        $("body").css("background-image", "url("+gallery[5]+")");
+        renderImage(gallery[5]);
         break;   
       case id >= 600 && id <= 622: // snow
-        $("body").css("background-image", "url("+gallery[6]+")");
+        renderImage(gallery[6]);
         break;     
       default: // clear skies
-        $("body").css("background-image", "url("+gallery[0]+")")
+        renderImage(gallery[0]);
     }
   }
-  
 
+  function renderImage(image) {
+    $('<img/>').attr('src', image).on('load', function() { // create image in memory and call function on load event
+      $(this).remove(); // prevent memory leaks
+      $('body').css('background-image', "url("+image+")");
+      showPage();
+    });
+  }
+  
+  function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("weather-box").style.display = "block";
+  }
   
   // Toggle from fahrenheit to celsius
   $("#unit").click(function() {
@@ -111,10 +118,5 @@ $(document).ready(function(){
   function getFahrenheit(celsius) {
     return Math.round(celsius * 1.8 + 32);
   }
-
-
-
-
-  getLocation();
 
 });
